5c2f1ed9d766ae4cb88c4ad585ee65eeabd169c7
Andre Oliveira <fulcrunn@yahoo.com.br>
2025-10-14 18:04:03 -0300

    apiGateway

---
+++ apiGateway/gateway.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan('dev'));
+app.use(cors());
+
+// 🔹 Rota para o microserviço de clientes
+app.use('/api/clientes', createProxyMiddleware({
+  target: 'http://localhost:8080/api/clientes',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', // 
+  },
+  //pathRewrite: { '^/api/clientes': '' },
+}));
+
+// 🔹 Rota principal (teste)
+app.get('/', (req, res) => {
+  res.send('Gateway ativo e roteando requisições!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 API Gateway rodando em http://localhost:${PORT}`);
+});
